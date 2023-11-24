5cb5a54d2742b5f438f4c392ec6fb94d6940f67e
Gabriel Williams <81557989+Gab-williams@users.noreply.github.com>
2023-11-24 23:06:54 +0100

    Update Ig_Grid.js

--- src/Ig_Grid.js
+++ src/Ig_Grid.js
@@ -1,35 +1,56 @@
-import React,{useEffect, useState} from 'react'
-import Header from './components/main/Header'
-import Body from './components/Ig_Grid/Body'
-import MobileNav from './components/Home/MobileNav'
-import Footer from './components/main/Foooter'
+import React, { useEffect, useState } from 'react';
+import Header from './components/main/Header';
+import Body from './components/Ig_Grid/Body';
+import MobileNav from './components/Home/MobileNav';
+import Footer from './components/main/Foooter';
 
 export default function Ig_Grid() {
-    const [sunmoon, setSunMoon] = useState(false)
+  const [sunmoon, setSunMoon] = useState(false);
+  const [items, setItems] = useState([]);
+  const [visibleItems, setVisibleItems] = useState(3); // Number of items to initially display
 
-    // const handleMoon =(e)=>{
-    //     e.preventDefault();
-    //  setSunMoon(true)  
-    //  document.body.style='background:black;'; 
-    // }
-    
-    const handleSun =(e)=>{
-        if(!sunmoon){
+  // Mock data, replace this with your actual data fetching logic
+  const fetchData = () => {
+    // Mock data fetching, replace this with your actual data fetching logic
+    const data = [
+      // Your data items go here
+    ];
+    setItems(data);
+  };
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetchData();
+  }, []);
+
+  const handleSun = (e) => {
+    if (!sunmoon) {
       e.preventDefault();
-     setSunMoon(true)  
-     document.body.style='background:black;'; 
-        }else{
-        e.preventDefault()
-        setSunMoon(false)
-        document.body.style = 'background:white;';
-        }
+      setSunMoon(true);
+      document.body.style = 'background:black;';
+    } else {
+      e.preventDefault();
+      setSunMoon(false);
+      document.body.style = 'background:white;';
     }
-    return (
-        <div className='w-full'>
-            <Header sunmoon={sunmoon} setSunMoon={setSunMoon}  handleSun={handleSun}/>
-            <MobileNav/>
-             <Body/>
-             <Footer/>
-        </div>
-    )
+  };
+
+  const handleLoadMore = () => {
+    // Increase the number of visible items on "Load More" button click
+    setVisibleItems((prevVisibleItems) => prevVisibleItems + 3); // Increase by 3 items, you can adjust as needed
+  };
+
+  return (
+    <div className='w-full'>
+      <Header sunmoon={sunmoon} setSunMoon={setSunMoon} handleSun={handleSun} />
+      <MobileNav />
+      <Body items={items.slice(0, visibleItems)} />
+      {items.length > visibleItems && (
+        <button onClick={handleLoadMore} className='load-more-button'>
+          Load More
+        </button>
+      )}
+      <Footer />
+    </div>
+  );
 }
