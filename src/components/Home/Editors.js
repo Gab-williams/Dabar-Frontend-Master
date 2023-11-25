e51cd7c9666849179b6094b9207c0c6451c087eb
Gabriel Williams <81557989+Gab-williams@users.noreply.github.com>
2023-11-25 01:03:02 +0100

    Update Editors.js

--- src/components/Home/Editors.js
+++ src/components/Home/Editors.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import picone from "../../images/cell-phone-pink-background-shows-analytics-graphics-generative-ai_563818-6612 1.png";
 import pictwo from "../../images/people.png";
@@ -7,7 +7,7 @@ import picfour from "../../images/robot.png";
 import picfive from "../../images/medical.png";
 
 export default function Editors(props) {
-  const {  editorall, client, handleClick } = props;
+  const { editorall, client, handleClick } = props;
   // const arr = [
   //   {
   //     id: 1,
@@ -15,9 +15,9 @@ export default function Editors(props) {
   //     author: "alec whitten",
   //     date: "1 jan 2023",
   //     pic: picone,
-  //     heading: `payaza launches new crypto account and 
+  //     heading: `payaza launches new crypto account and
   //           card for use in Nigeria`,
-  //     body: `Like to Know the secret of transforming a 
+  //     body: `Like to Know the secret of transforming a
   //           2-14 into Super Bowl winning Dynasty`,
   //   },
   //   {
@@ -28,7 +28,7 @@ export default function Editors(props) {
   //     pic: pictwo,
   //     heading: `Federal Reserve Announces Interest Rate Hike citing
   //           inflation concern`,
-  //     body: `Like to Know the secret of transforming a 
+  //     body: `Like to Know the secret of transforming a
   //           2-14 into Super Bowl winning Dynasty`,
   //   },
   //   {
@@ -39,7 +39,7 @@ export default function Editors(props) {
   //     pic: picthree,
   //     heading: `Federal Reserve Announces Interest Rate Hike citing
   //           inflation concern`,
-  //     body: `Like to Know the secret of transforming a 
+  //     body: `Like to Know the secret of transforming a
   //           2-14 into Super Bowl winning Dynasty`,
   //   },
   //   {
@@ -50,50 +50,49 @@ export default function Editors(props) {
   //     pic: picfour,
   //     heading: `Federal Reserve Announces Interest Rate Hike citing
   //           inflation concern`,
-  //     body: `Like to Know the secret of transforming a 
+  //     body: `Like to Know the secret of transforming a
   //           2-14 into Super Bowl winning Dynasty`,
   //   },
   // ];
 
-
-  const [alldata, setallData] = useState([])  
+  const [alldata, setallData] = useState([]);
   //console.log(featurestories)
-  useEffect(()=>{
+  useEffect(() => {
+    const fetchData = async () => {
+      const newData = await Promise.all(
+        editorall.map(async (item) => {
+          console.log(item.fields.storyId.fields);
+          let data = await client.getEntry(
+            item.fields.storyId.fields.categoryId.sys.id
+          );
+          let writer = await client.getEntry(
+            item.fields.storyId.fields.writerId.sys.id
+          );
+          let answer = data.fields.category;
+          let answriter = writer.fields.name;
+          return {
+            heading: item.fields.storyId.fields.heading,
+            summary: item.fields.storyId.fields.summary,
+            category: answer,
+            thumbnail: item.fields.storyId.fields.thumbnail.fields.file.url,
+            writer: answriter,
+            id: item.sys.id,
+          };
+        })
+      );
 
-     const fetchData = async () => {
-        const newData = await Promise.all(
-          editorall.map(async (item) => {
-             console.log(item.fields.storyId.fields)
-            let data = await client.getEntry(item.fields.storyId.fields.categoryId.sys.id);
-            let writer = await client.getEntry(item.fields.storyId.fields.writerId.sys.id)
-            let answer = data.fields.category;
-             let answriter = writer.fields.name
-            return {
-              heading: item.fields.storyId.fields.heading,
-              summary: item.fields.storyId.fields.summary,
-              category: answer,
-              thumbnail:item.fields.storyId.fields.thumbnail.fields.file.url,
-              writer:answriter,
-              id:item.sys.id
-            };
-          })
-        );
-  
-        setallData(newData);
-      };
-  
-      fetchData();
+      setallData(newData);
+    };
 
-  },[editorall])
-
-
+    fetchData();
+  }, [editorall]);
 
   return (
     <div className="w-full flex flex-col items-center">
-      <section className="w-10/12  ">
+      <section className="w-11/12  ">
         <article className="w-full flex flex-row items-center justify-between mt-20">
           <span className="w-1/2 sm:w-1/2 md:w-1/2 lg:w-1/6 ">
-            <button className="capitalize font-normal border-l-4 border-[#FD9005] text-sm px-1 ">
+            <button className="capitalize font-normal text-xl text-sm px-2 hidden md:inline">
               Editors Pick
             </button>
           </span>
@@ -106,42 +105,58 @@ export default function Editors(props) {
           </button>
         </article>
       </section>
-      <div className="w-10/12 mt-4">
-      <article className=" w-full grid grid-cols-1 gap-9 place-content-center sm:w-full sm:grid sm:grid-cols-1 sm:gap-9  sm:place-content-between  md:w-full md:grid md:grid-cols-1 md:gap-3 md:space-x-4 md:place-content-center lg:w-full lg:grid lg:grid-cols-4 lg:gap-y-14 lg:space-x-4 lg:place-content-center">
+      <div className="w-11/12 mt-4">
+        <article className=" w-full grid grid-cols-1 gap-4 place-content-center sm:w-full sm:grid sm:grid-cols-1 sm:gap-9  sm:place-content-between  md:w-full md:grid md:grid-cols-1 md:gap-3 md:space-x-4 md:place-content-center lg:w-full lg:grid lg:grid-cols-4 lg:gap-y-14 lg:space-x-2 lg:place-content-center">
           {alldata.map((item, index) => {
             return (
               <section
-                className="m-auto w-full sm:m-auto sm:w-full md:m-auto md:w-full lg:w-64"
+                className="m-auto min-h-full w-full sm:m-auto sm:w-full md:m-auto md:w-full lg:w-80"
                 key={index}
-                onClick={()=>handleClick(item.id)}
+                onClick={() => handleClick(item.id)}
               >
                 <div className=" w-full flex flex-row space-x-4 items-center p-1 sm:w-full sm:space-x-1 sm:flex sm:flex-row sm:items-center sm:p-1 md:w-full md:space-x-1 md:flex md:flex-row md:items-center md:p-1 lg:w-full lg:flex lg:flex-col lg:items-center lg:p-1">
                   <section className="   w-56 relative sm:w-56 sm:relative md:w-60  md:relative  lg:w-full lg:relative">
                     <img
                       src={item.thumbnail}
-                      className="w-full h-24 sm:h-24 md:h-32 lg:h-40"
+                      className="w-full h-24 sm:h-auto md:h-32 lg:h-auto"
                     />
                     <article className="w-full absolute top-0 left-0 right-0 bottom-0 bg-cover bg-black bg-opacity-10">
-                      <div className="w-full mt-4 ml-3">
-                        <button className="px-2.5 py-1 font-medium capitalize text-white bg-[#FD9005] text-xs hidden sm:block md:block lg:block">
-                          {item.category}
-                        </button>
-                      </div>
+                      
                     </article>
                   </section>
 
                   <section className="w-full flex flex-col items-center  sm:w-full sm:flex sm:flex-col sm:items-center md:w-11/12 md:flex md:flex-col md:items-center  lg:w-full lg:flex lg:flex-col lg:items-center">
-                    <div className="w-full mt-4">
-                    <span className='flex flex-row w-full sm:w-full md:w-full lg:w-3/4 float-left capitalize font-medium text-xs space-x-1'><a>{item.writer.length >= 15?item.writer.substr(0, 20)+"...":item.writer }</a> <a>.</a> <a>1 jan 2023</a></span>
+                   
+                    <div className="w-full mt-2">
+                        <button className=" font-medium capitalize text-orange-500 text-xs hidden sm:block md:block lg:block">
+                          {item.category}
+                        </button>
+                      </div>
 
-                    </div>
-
-                    <section className="w-full text-xs mt-1 sm:text-xs sm:mt-1 md:text-xs md:mt-1 lg:text-xs lg:mt-3 font-semibold text-left capitalize" onClick={()=>handleClick(item.id)}>
-                      {item.heading.length >= 70?item.heading.substr(0,100)+"...":item.heading}
+                    <section
+                      className="w-full text-xs mt-1 sm:text-xs sm:mt-1 md:text-xs md:mt-1 lg:text-sm lg:mt-3 font-semibold text-left capitalize"
+                      onClick={() => handleClick(item.id)}
+                    >
+                      {item.heading.length >= 70
+                        ? item.heading.substr(0, 100) + "..."
+                        : item.heading}
                     </section>
+                    
 
                     <article className="text-xs mt-1 hidden sm:block md:hidden lg:block lg:text-xs lg:mt-3 w-full text-left capitalize text-gray-500">
-                    {item.summary.length >= 100?item.summary.substr(0, 90):item.summary}
+                      {item.summary.length >= 100
+                        ? item.summary.substr(0, 90)
+                        : item.summary}
+                         <div className="w-full mt-4">
+                      <span className="flex flex-row w-full sm:w-full md:w-full lg:w-3/4 float-left capitalize font-medium text-xs space-x-1">
+                        <a>
+                          {item.writer.length >= 15
+                            ? item.writer.substr(0, 20) + "..."
+                            : item.writer}
+                        </a>{" "}
+                        <a>.</a> <a>1 jan 2023</a>
+                      </span>
+                    </div>
                     </article>
                   </section>
                 </div>
