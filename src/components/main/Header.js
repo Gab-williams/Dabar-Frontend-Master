002152409ba7373295b6316556f9b4ce85199b8c
Gab-williams <81557989+Gab-williams@users.noreply.github.com>
2023-11-26 12:37:15 +0100

    Update Header.js

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -23,7 +23,50 @@ export default function Header(props) {
   const handleCategoriesToggle = () => {
     setIsCategoriesOpen(!isCategoriesOpen);
   };
-
+  const categoryMenu = [
+    {
+      title: "Business Insights",
+      subcategories: [
+        "Industry Insights",
+        "Entrepreneurship",
+        "Leadership & Management",
+        "Global Economy",
+        "Corporate Responsibility",
+      ],
+    },
+    {
+      title: "Technology Trends",
+      subcategories: [
+        "Technology & Innovation",
+        "Blockchain & Cryptocurrency",
+        "Artificial Intelligence & Machine Learning",
+      ],
+    },
+    {
+      title: "Marketing & Finance",
+      subcategories: ["Marketing & Advertising", "Finance & Investment"],
+    },
+    {
+      title: "Workplace & Culture",
+      subcategories: [
+        "Human Resources",
+        "Diversity & Inclusion",
+        "Career Development",
+      ],
+    },
+    {
+      title: "Productivity & Innovation",
+      subcategories: ["Productivity & Tools", "Innovation Implementation"],
+    },
+    {
+      title: "Multimedia & Events",
+      subcategories: ["Events & Conferences", "Multimedia Content"],
+    },
+    {
+      title: "Opinions & Editorials",
+      subcategories: ["Opinion & Editorial"],
+    },
+  ];
   const [ispages, setisPage] = useState(false);
   const handleisPage = () => {
     if (!ispages) {
@@ -40,87 +83,182 @@ export default function Header(props) {
   // }
   // },[]);
 
-    return (
-        <div className=" md:w-full md:flex md:flex-col md:items-center md:justify-center md:p-2 md:headerall lg:w-full lg:flex lg:items-center lg:justify-center lg:p-2 headerall">
-            {/* large screen  */}
-              <article className={!sunmoon?'hidden sm:hidden md:hidden lg:w-10/12  lg:flex lg:flex-row lg:items-center lg:justify-between  lg:border-b lg:border-gray lg:p-6':'hidden sm:hidden md:hidden lg:w-10/12  lg:flex lg:flex-row lg:items-center lg:justify-between  lg:border-b lg:border-white lg:p-6'} >
-                {/* logo and links */}
-                 <section className='w-1/2'>
-                   <div className='w-full flex flex-row items-center justify-between'>
-                    
-                       <span className='w-1/5  '>
-                       <Link to="/">
-                         <img src={!sunmoon?logo:logotwo} className='w-full h-full' />
-                         </Link>
-                       </span>
+  return (
+    <div className=" md:w-full md:flex md:flex-col md:items-center md:justify-center md:p-2 md:headerall lg:w-full lg:flex lg:items-center lg:justify-center lg:p-2 headerall">
+      {/* large screen  */}
+      <article
+        className={
+          !sunmoon
+            ? "hidden sm:hidden md:hidden lg:w-10/12  lg:flex lg:flex-row lg:items-center lg:justify-between  lg:border-b lg:border-gray lg:p-6"
+            : "hidden sm:hidden md:hidden lg:w-10/12  lg:flex lg:flex-row lg:items-center lg:justify-between  lg:border-b lg:border-white lg:p-6"
+        }
+      >
+        {/* logo and links */}
+        <section className="w-1/2">
+          <div className="w-full flex flex-row items-center justify-between">
+            <span className="w-1/5  ">
+              <Link to="/">
+                <img
+                  src={!sunmoon ? logo : logotwo}
+                  className="w-full h-full"
+                />
+              </Link>
+            </span>
 
-                       <span className='w-3/4'>
-                         <ul className='w-full flex flex-row items-center  space-x-4'>
-                              <li>
-                                <span className='flex flex-row space-x-1'>
-                                <a className=' font-normal capitalize text-base'>Catagories</a><a className='grid place-content-center'><MdKeyboardArrowDown className='text-base'/></a>
-                                </span>
-                                </li>
-                              <li>
-                              <span className='flex flex-row space-x-1 relative'>
-                                <a onClick={()=>handleisPage()} className=' font-normal capitalize text-base cursor-pointer'>Pages</a><a onClick={()=>handleisPage()}  className='grid place-content-center'><MdKeyboardArrowDown className='text-base'/></a>
+            <span className="w-3/4">
+              <ul className="w-full flex flex-row items-center  space-x-4">
+                <li>
+                  <span className="flex flex-row space-x-1 relative">
+                    <a
+                      onClick={() => handleCategoriesToggle()}
+                      className=" font-normal capitalize text-base cursor-pointer"
+                    >
+                      Catagories
+                    </a>
+                    <a
+                      onClick={() => handleCategoriesToggle()}
+                      className="grid place-content-center"
+                    >
+                      <MdKeyboardArrowDown className="text-base" />
+                    </a>
+                    <ul className={isCategoriesOpen ? 'mega-menu grid grid-flow-col auto-cols-max grid-rows-2 gap-4 rounded-sm bg-white top-7 absolute items-start ease-in-out duration-300 shadow-md z-20 p-4' : 'h-0 hidden ease-in-out duration-300 z-0'}>
+  {categoryMenu.map((category, index) => (
+    <li key={index} className='mb-4'>
+      <Link to={`/${category.title.replace(/\s+/g, '-').toLowerCase()}`} className='font-bold text-md text-black'>{category.title}</Link>
+      {category.subcategories && category.subcategories.length > 0 && (
+        <ul className='w-40 sub-menu text-xs gap-2 mt-2'>
+          {category.subcategories.map((subcategory, subIndex) => (
+            <li key={subIndex}><Link to={`/${subcategory.replace(/\s+/g, '-').toLowerCase()}`} className='text-gray-600'>{subcategory}</Link></li>
+          ))}
+        </ul>
+      )}
+    </li>
+  ))}
+</ul>
+                  </span>
+                </li>
+                <li>
+                  <span className="flex flex-row space-x-1 relative">
+                    <a
+                      onClick={() => handleisPage()}
+                      className=" font-normal capitalize text-base cursor-pointer"
+                    >
+                      Pages
+                    </a>
+                    <a
+                      onClick={() => handleisPage()}
+                      className="grid place-content-center"
+                    >
+                      <MdKeyboardArrowDown className="text-base" />
+                    </a>
 
-                                <ul className={ispages?'w-36 rounded-sm bg-white top-7 absolute flex flex-col items-center ease-in-out duration-300  shadow-md z-20':'h-0 hidden ease-in-out duration-300 z-0'} >
-                                  <li className='w-full p-1 text-sm text-left font-medium capitalize text-black'><Link to="/">Home</Link> </li>
-                                  <li className='w-full p-1 text-sm text-left font-medium capitalize text-black'><Link to="/story">story</Link></li>
-                                  <li className='w-full p-1 text-sm text-left font-medium capitalize text-black'><Link to="/about us">about us</Link></li>
-                                  <li className='w-full p-1 text-sm text-left font-medium capitalize text-black'><Link to="/team">team</Link></li>
-                                  <li className='w-full p-1 text-sm text-left font-medium capitalize text-black'><Link to="/video-category">video category</Link></li>
-                                  <li className='w-full p-1 text-sm text-left font-medium capitalize text-black'><Link to="/category-list">category list</Link></li>
-                                  {/* Ig-Grid */}
-                                  <li className='w-full p-1 text-sm text-left font-medium capitalize text-black'><Link to="/ig_grid">ig grid</Link></li>
-                                  <li className='w-full p-1 text-sm text-left font-medium capitalize text-black'><Link to="/contact us">contact us</Link></li>
-                                </ul>
-                                </span>
-                              </li>
-                              <li>
-                              <span className='flex flex-row space-x-1'>
-                                <a className=' font-normal capitalize text-base'><Link to="/contact us">Contact us</Link></a>
-                                </span>
-                              </li>
-                              <li>
-                              <span className='flex flex-row space-x-1'>
-                                <a className=' font-normal capitalize text-base'><Link to="/about us">About us</Link></a>
-                                </span>
-                              </li>
-                         </ul>
-                       </span>
+                    <ul
+                      className={
+                        ispages
+                          ? "w-36 rounded-sm bg-white top-7 absolute flex flex-col items-center ease-in-out duration-300  shadow-md z-20"
+                          : "h-0 hidden ease-in-out duration-300 z-0"
+                      }
+                    >
+                      <li className="w-full p-1 text-sm text-left font-medium capitalize text-black">
+                        <Link to="/">Home</Link>{" "}
+                      </li>
+                      <li className="w-full p-1 text-sm text-left font-medium capitalize text-black">
+                        <Link to="/story">story</Link>
+                      </li>
+                      <li className="w-full p-1 text-sm text-left font-medium capitalize text-black">
+                        <Link to="/about us">about us</Link>
+                      </li>
+                      <li className="w-full p-1 text-sm text-left font-medium capitalize text-black">
+                        <Link to="/team">team</Link>
+                      </li>
+                      <li className="w-full p-1 text-sm text-left font-medium capitalize text-black">
+                        <Link to="/video-category">video category</Link>
+                      </li>
+                      <li className="w-full p-1 text-sm text-left font-medium capitalize text-black">
+                        <Link to="/category-list">category list</Link>
+                      </li>
+                      {/* Ig-Grid */}
+                      <li className="w-full p-1 text-sm text-left font-medium capitalize text-black">
+                        <Link to="/ig_grid">ig grid</Link>
+                      </li>
+                      <li className="w-full p-1 text-sm text-left font-medium capitalize text-black">
+                        <Link to="/contact us">contact us</Link>
+                      </li>
+                    </ul>
+                  </span>
+                </li>
+                <li>
+                  <span className="flex flex-row space-x-1">
+                    <a className=" font-normal capitalize text-base">
+                      <Link to="/contact us">Contact us</Link>
+                    </a>
+                  </span>
+                </li>
+                <li>
+                  <span className="flex flex-row space-x-1">
+                    <a className=" font-normal capitalize text-base">
+                      <Link to="/about us">About us</Link>
+                    </a>
+                  </span>
+                </li>
+              </ul>
+            </span>
+          </div>
+        </section>
+        {/* logo and links */}
 
-                    </div> 
-                 </section>
-                  {/* logo and links */}
+        {/* search bar and toogle */}
+        <section className="w-2/5 ">
+          <div className="w-full flex flex-row items-center space-x-5">
+            <article
+              className={
+                !sunmoon
+                  ? "w-2/3 bg-[#F5F5F5] rounded-md px-1 py-1"
+                  : "w-2/3 bg-black rounded-md px-1 py-1"
+              }
+            >
+              <div className="w-full flex flex-row items-center space-x-1">
+                <a className="w-1/12 grid place-content-center">
+                  <BsThreeDotsVertical className="w-full h-full text-base" />
+                </a>
+                <span className="w-10/12">
+                  <input
+                    type="text"
+                    className="w-full h-full bg-[#F5F5F5] outline-none border-0 p-1"
+                  />
+                </span>
+                <a className="w-1/12 grid place-content-center">
+                  <BsSearch className="w-full h-full text-sm" />
+                </a>
+              </div>
+            </article>
 
-                   
-                   {/* search bar and toogle */}
-                 <section className='w-2/5 '>
-                    <div className='w-full flex flex-row items-center space-x-5'>
-                     <article className= {!sunmoon?'w-2/3 bg-[#F5F5F5] rounded-md px-1 py-1':'w-2/3 bg-black rounded-md px-1 py-1'}>
-                       <div className='w-full flex flex-row items-center space-x-1'>
-                         <a className='w-1/12 grid place-content-center'>
-                          <BsThreeDotsVertical className="w-full h-full text-base"/>
-                         </a>
-                         <span className='w-10/12'>
-                            <input type='text' className='w-full h-full bg-[#F5F5F5] outline-none border-0 p-1'/>
-                         </span>
-                         <a className='w-1/12 grid place-content-center'>
-                            <BsSearch className="w-full h-full text-sm"/>
-                         </a>
-                       </div>
-                     </article>
-                         
-                         <div  onClick={(e)=>handleSun(e)} className={!sunmoon?'w-1/6 border-2  border-black relative rounded-full change':'w-1/6 border-2 border-white relative rounded-full changex'}>
-                            <article className='w-full flex flex-row items-center  justify-between relative p-1 '>
-                                  <button className='w-1/3 h-full ' >
-                                     <MdOutlineWbSunny className={sunmoon?"w-full h-full text-2xl text-white":"w-full h-full text-2xl text-black"} />
-                                  </button>
-                                  <button  onClick={(e)=>handleSun(e)} className= {!sunmoon?'rounded-full w-6 h-6 circle':'rounded-full w-6 h-6 circlex'} >
-
-                                  </button>
+            <div
+              onClick={(e) => handleSun(e)}
+              className={
+                !sunmoon
+                  ? "w-1/6 border-2  border-black relative rounded-full change"
+                  : "w-1/6 border-2 border-white relative rounded-full changex"
+              }
+            >
+              <article className="w-full flex flex-row items-center  justify-between relative p-1 ">
+                <button className="w-1/3 h-full ">
+                  <MdOutlineWbSunny
+                    className={
+                      sunmoon
+                        ? "w-full h-full text-2xl text-white"
+                        : "w-full h-full text-2xl text-black"
+                    }
+                  />
+                </button>
+                <button
+                  onClick={(e) => handleSun(e)}
+                  className={
+                    !sunmoon
+                      ? "rounded-full w-6 h-6 circle"
+                      : "rounded-full w-6 h-6 circlex"
+                  }
+                ></button>
 
                 <button className="w-1/3 h-full ">
                   <BsMoon
