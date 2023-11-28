11b43302c583eae7cde33890d96287e17ba57b3c
daroOkpeku <stephenjason41@gmail.com>
2023-11-28 13:26:40 +0100

    all issues fixed

--- src/components/Story/Body.js
+++ src/components/Story/Body.js
@@ -6,9 +6,9 @@ export default function Body(props) {
         <div className='w-full flex flex-col items-center mt-5'>
           {para.map((item)=>{
             return item.content.map((one, index)=>{
-              return<p className='text-md text-left capitalize font-regular mt-2' key={index}>
+              return<div className='text-md text-left capitalize font-regular mt-2' key={index}>
                 {one.value}
-              </p>
+              </div>
             })
           })}
             
